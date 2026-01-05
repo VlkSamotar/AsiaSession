08ff86444b1f08a881037737e22c168fb0a39523
VlkSamotar <spravce@vlksamotar.cz>
2026-01-05 19:40:23 +0100

    Add and implement PossibleTimeFrames enum.

---
+++ AsiaSession/Enums/PossibleTimeFrames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsiaSession.Enums
+{
+	public enum PossibleTimeFrames
+	{
+		Minute1,
+		Minute5,
+		Minute15,
+		Minute30,
+		Hour1,
+		Hour4
+	}
+}
---
+++ AsiaSession/Helpers/TimeFrameMapper.cs
@@ -0,0 +1,22 @@
+﻿using AsiaSession.Enums;
+using cAlgo.API;
+
+namespace AsiaSession.Helpers
+{
+	public static class TimeFrameMapper
+	{
+		public static TimeFrame FromPossibleTimeFrames(PossibleTimeFrames possibleTimeFrames)
+		{
+			return possibleTimeFrames switch
+			{
+				PossibleTimeFrames.Minute1 => TimeFrame.Minute,
+				PossibleTimeFrames.Minute5 => TimeFrame.Minute5,
+				PossibleTimeFrames.Minute15 => TimeFrame.Minute15,
+				PossibleTimeFrames.Minute30 => TimeFrame.Minute30,
+				PossibleTimeFrames.Hour1 => TimeFrame.Hour,
+				PossibleTimeFrames.Hour4 => TimeFrame.Hour4,
+				_ => TimeFrame.Hour,
+			};
+		}
+	}
+}
